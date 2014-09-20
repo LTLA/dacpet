ee025530f5cdad0e2a9e3d1f307f661c14d17361
Aaron Lun <alun@wehi.edu.au>
2014-09-20 11:18:04 +1000

    Added path.expand to all h5 utilities.

--- package/R/h5utils.R
+++ package/R/h5utils.R
@@ -7,6 +7,7 @@ lengthstr <- 'lengths'
 # each chromosome combination for each library. 
 {
 	overall <- list()
+	y <- path.expand(y)
 	ni<-length(y)
 	for (ix in 1:ni) {
 		current <- h5ls(y[ix])
@@ -28,11 +29,18 @@ lengthstr <- 'lengths'
 	return(overall)
 }
 
-.getPairs <- function(y, anchor, target) { h5read(y, file.path(countstr, anchor, target)) }
+.getPairs <- function(y, anchor, target) { 
+	y <- path.expand(y)
+	h5read(y, file.path(countstr, anchor, target)) 
+}
 
-.getLengths <- function(y) { h5read(y, lengthstr) }
+.getLengths <- function(y) { 
+	y <- path.expand(y)
+	h5read(y, lengthstr) 
+}
 
 .initializeH5 <- function(y, len) {
+	y <- path.expand(y)
 	if (file.exists(y)) { unlink(y, recursive=TRUE) } 
 	if (!h5createFile(y)) { stop(sprintf("failed to create '%s'", y)) }
 	if (!h5createGroup(y, countstr)) { stop(sprintf("failed to add 'counts' group to '%s'", y)) }
@@ -41,11 +49,13 @@ lengthstr <- 'lengths'
 }
 
 .addGroup <- function(y, anchor) {
+	y <- path.expand(y)
 	if (!h5createGroup(y, file.path(countstr, anchor))) { stop("failed to add '%s' group to '%s'", anchor, y) }
 	return(invisible(NULL))
 }
 
 .writePairs <- function(y, anchor, target, pairs) {
+	y <- path.expand(y)
 	if (h5write(pairs, y, file.path(countstr, anchor, target))) { stop("failed to add tag pair data to '%s'", y) }
 	return(invisible(NULL))
 }
