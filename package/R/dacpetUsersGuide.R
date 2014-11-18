9bd37a24840ea09e425376401bf5fd417bd3dce8
Aaron Lun <alun@wehi.edu.au>
2014-11-19 09:11:36 +1100

    Added function to bring up UG, updated UG.

---
+++ package/R/dacpetUsersGuide.R
@@ -0,0 +1,16 @@
+dacpetUsersGuide <- function(view=TRUE)
+# Find, and optionally show, the diffHic user's guide 
+# 
+# written by Aaron Lun, based on equivalent from edgeR by Gordon Smyth
+# 7 November 2014.
+{
+	ugloc <- system.file("doc", "dacpetUG.pdf", package="dacpet")
+	if (view) {
+		if(.Platform$OS.type == "windows") {
+			shell.exec(ugloc)
+		} else {
+			system(paste(Sys.getenv("R_PDFVIEWER"),ugloc,"&"))
+		}
+	}
+	return(ugloc)
+}
